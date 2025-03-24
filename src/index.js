a7b822c9323e2470071674484ef6f8492f6b529c
Ian Sang <sangian006@gmail.com>
2025-03-24 14:58:01 +0300

    Some content added to index.js, a div element created in my html

--- src/index.js
+++ src/index.js
@@ -12,3 +12,48 @@ fetch("http://localhost:3000/recipes", options)
   .then((response) => response.json())
   .then((response) => console.log(response))
   .catch((err) => console.error(err));
+
+function displayRecipes(recipes) {
+  const container = document.getElementById("recipe-container");
+
+  //clear container content
+  container.innerHTML = "";
+
+  recipes.forEach((recipe) => {
+    //for each recipe create a new div element
+    const recipeElement = document.createElement("div");
+    recipeElement.classList.add("recipe");
+
+    //create elements for recipe name, image, ingredients and instructions
+    const title = document.createElement("h2");
+    title.textContent = recipe.name;
+
+    const image = document.createElement("img");
+    image.src = recipe.image;
+    image.alt = recipe.name;
+
+    const ingredientsList = document.createElement("ul");
+    recipe.ingredients.forEach((ingredients) => {
+      const li = document.createElement("li");
+      li.textContent = ingredient;
+      ingredientsList.appendChild(li);
+    });
+
+    //Append elements to the recipe element
+    recipeElement.appendChild(title);
+    recipeElement.appendChild(image);
+    recipeElement.appendChild(ingredientsList);
+
+    //Add instructions as a list
+    const instructionList = document.createElement("ol");
+    recipe.instructions.forEach((step) => {
+      const li = document.createElement("li");
+      li.textContent = step;
+      instructionList.appendChild(li);
+    });
+    recipeElement.appendChild(instructionList);
+
+    //Append the recipe element to the container
+    container.appendChild(recipeElement);
+  });
+}
